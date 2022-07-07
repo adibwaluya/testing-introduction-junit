ccca71266beb3387e18cb26cef6ac5b322d799aa
rz-p <pramadhan9@gmail.com>
2022-07-07 15:50:00 +0200

    added calculateAvg

--- GradingScale.java
+++ GradingScale.java
@@ -1,45 +1,54 @@
 public class GradingScale {
 
-    private double grade;
+    private double total;
     private int count;
+    private double average;
 
     public String calculateGrade(double points) {
         String ects = " ";
-        grade = 0.0;
+        total = 0.0;
         count = 0;
 
-        if(points <= 90 && points >= 100) {
-            grade += points;
-            count++;
-            ects = "A";
-        }
-        if(points <= 75 && points >= 89) {
-            grade += points;
-            count++;
-            ects = "B";
-        }
-        if(points <= 60 && points >= 74) {
-            grade += points;
-            count++;
-            ects = "C";
-        }
-        if(points <= 50 && points >= 59) {
-            grade += points;
-            count++;
-            ects = "D";
-        }
-        if(points < 50) {
-            grade += points;
-            count++;
-            ects = "F";
+        while(total != 0) {
+            if(points <= 90 && points >= 100) {
+                total += points;
+                count++;
+                ects = "A";
+            }
+            if(points <= 75 && points >= 89) {
+                total += points;
+                count++;
+                ects = "B";
+            }
+            if(points <= 60 && points >= 74) {
+                total += points;
+                count++;
+                ects = "C";
+            }
+            if(points <= 50 && points >= 59) {
+                total += points;
+                count++;
+                ects = "D";
+            }
+            if(points < 50) {
+                total += points;
+                count++;
+                ects = "F";
+            }
+
+            if(points == -1) {
+                calculateAvg();
+                break;
+            }
         }
 
         return ects;
 
     }
 
-    public double calculateAvg(double grade, int count) {
-        return grade / count;
+    public double calculateAvg() {
+        average = total / count;
+        return average;
     }
 
 
--- Test/GradingScaleTest.java
+++ Test/GradingScaleTest.java
@@ -2,14 +2,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.After;
 
 public class GradingScaleTest {
 
     @Test
-    void Calculate() {
+    static void Calculate() {
         GradingScale gradingScale = new GradingScale();
         assertEquals("A", gradingScale.calculateGrade(90));
+        assertEquals("B", gradingScale.calculateGrade(83));
+        assertEquals("C", gradingScale.calculateGrade(65));
+        assertEquals("D", gradingScale.calculateGrade(51));
+        assertEquals("D", gradingScale.calculateGrade(43));
+
+        assertEquals(297.6, gradingScale.calculateAvg());
+
+
     }
+
 }
