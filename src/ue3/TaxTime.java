2d2191b78129955172fb7b64ef7653138c736ae0
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2022-07-17 10:31:28 +0200

    implemented white box tests for ex A 1, 2, 3

---
+++ src/ue3/TaxTime.java
@@ -0,0 +1,109 @@
+package ue3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TaxTime {
+    static int nFamilyMembers;
+    static int famMember;
+    static int exemption;
+    static double income;
+    static double taxTotal;
+
+    public static void main(String[] args) {
+
+        // A Reader stream to read from the console
+        BufferedReader in =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println ("Welcome to the new Berlin tax calculator.");
+        System.out.print   ("How much did you earn last year? ");
+        try {
+            income = Double.parseDouble(in.readLine());
+        } catch (NumberFormatException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+//     check income
+//     independent path: 2
+        if (income < 0) {
+            System.out.println ("Even in Berlin, no one has a negative income!");
+            System.out.println ("Start over.");
+            System.exit (-1);
+        }
+
+        System.out.print("Enter the number of dependents you have, including yourself: ");
+        try {
+            String s = in.readLine();
+            nFamilyMembers = Integer.valueOf(s).intValue();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//     check number of family members
+//     independent path: 2
+        if (nFamilyMembers <= 0) {
+            System.out.println("Did you forget to count yourself?");
+            System.out.println ("Start over.");
+            System.exit (-1);
+        }
+
+//    compute tax total
+//     independent path: 3
+        if (income < 10000)
+        taxTotal = 0.12 * income;
+      else if (income < 50000)
+        taxTotal = 300.00 + 0.24 * (income - 10000);
+      else
+        taxTotal = 1500.00 + 0.36 * (income - 50000);
+
+        for (int i = 0; i <= nFamilyMembers; i++){
+            taxTotal = taxTotal - 100;
+        }
+
+//    check negative tax
+//     independent path: 2
+        if (taxTotal < 0) // In case of negative tax
+        taxTotal=0;
+
+        System.out.println ("=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬");
+        System.out.println ("Berlin GmbH");
+        System.out.println ("Tax bill");
+        System.out.println ("Your income was " + income + " â‚¬.");
+        System.out.println ("You have " + nFamilyMembers + " family members.");
+        System.out.println ("Your total tax is " + taxTotal + " â‚¬.");
+        System.out.println ("Family member tax saving is " + nFamilyMembers*100 + "â‚¬.");
+        System.out.println ("=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬=â‚¬");
+    }
+
+    // Methods based on founded independent paths
+    String checkIncome(int income) {
+        if (income < 0) return "Even in Berlin, no one has a negative income!";
+        return "Correct income input";
+    }
+
+    String checkFamilyMember(int nFamilyMembers) {
+        if (nFamilyMembers <= 0) return "Did you forget to count yourself?";
+        return "Valid input";
+    }
+
+    String checkComputeTaxTotal(int income) {
+        famMember = 1;
+        if (income < 10000) {
+            taxTotal = 0.12 * income;
+            taxTotal = taxTotal - ((famMember+1)*100);
+            return "You have low income. Your tax is: " + taxTotal;
+        } else if (income < 50000) {
+            taxTotal = 300.00 + 0.24 * (income - 10000);
+            taxTotal = taxTotal - ((famMember+1)*100);
+            return "You have medium income. Your tax is: " + taxTotal;
+        } else {
+            taxTotal = 1500.00 + 0.36 * (income - 50000);
+            taxTotal = taxTotal - ((famMember+1)*100);
+            return "You have high income. Your tax is: " + taxTotal;
+        }
+    }
+}
---
+++ src/ue3/TaxTimeTest.java
@@ -0,0 +1,49 @@
+package ue3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaxTimeTest {
+    TaxTime taxTime;
+    int familyMember0;
+    int familyMember4;
+    int familyMember100;
+    int income0;
+    int income1;
+    int lowIncome;
+    int mediumIncome;
+    int highIncome;
+    //int minusTaxTotal;
+    //int normalTaxTotal;
+
+    @BeforeEach
+    void setUp() {
+        taxTime = new TaxTime();
+        income0 = -2;
+        income1 = 1;
+        lowIncome = 5000;
+        mediumIncome = 25000;
+        highIncome = 80000;
+    }
+
+    @Test
+    void familyAllIncome() {
+        //Test negative/positive income
+        assertEquals("Even in Berlin, no one has a negative income!", taxTime.checkIncome(income0));
+        assertEquals("Correct income input", taxTime.checkIncome(income1));
+
+        //Test family member
+        assertEquals("Did you forget to count yourself?", taxTime.checkFamilyMember(0));
+        assertEquals("Valid input", taxTime.checkFamilyMember(2));
+
+        //Test taxTotal lowIncome
+        assertEquals("You have low income. Your tax is: 400.0", taxTime.checkComputeTaxTotal(lowIncome));
+        //Test taxTotal mediumIncome
+        assertEquals("You have medium income. Your tax is: 3700.0", taxTime.checkComputeTaxTotal(mediumIncome));
+        //Test taxTotal HighIncome
+        assertEquals("You have high income. Your tax is: 12100.0", taxTime.checkComputeTaxTotal(highIncome));
+
+    }
+}
