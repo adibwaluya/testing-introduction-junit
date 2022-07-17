e7950d7d2d7fb2988b119a5d8ac5c2081da3b97c
rz-p <pramadhan9@gmail.com>
2022-07-17 12:03:14 +0200

    change test absolute checker

--- src/ue1/AbsolutValueCheckerTest.java
+++ src/ue1/AbsolutValueCheckerTest.java
@@ -7,34 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AbsolutValueCheckerTest {
 
-    int validValue = 0;
-    int invalidValue = 0;
-    int negativeValue = -1;
-    int positiveValue = 1;
+    int negativeValue;
+    int positiveValue;
 
     AbsolutValueChecker absolutValueChecker;
 
     @BeforeEach
     void setValue() {
         absolutValueChecker = new AbsolutValueChecker();
-        this.validValue = 5;
-        this.invalidValue = -5;
+        this.positiveValue = 5;
+        this.negativeValue = -5;
     }
 
     // Assert true for valid value (>0)
     @Test
-    void validAbsolutIntChecker() {
-        // 5 > -1
-        assertTrue(absolutValueChecker.absolutIntChecker(this.validValue) > negativeValue, "This value is valid");
-        // 5 < 1
-        assertFalse(absolutValueChecker.absolutIntChecker(this.validValue) < positiveValue, "This value is invalid!");
+    void positiveAbsolutIntChecker() {
+        // 5 == 5
+        assertTrue(absolutValueChecker.absolutIntChecker(this.positiveValue) == positiveValue, "This value should be positive");
+        // 5 == -5
+        assertFalse(absolutValueChecker.absolutIntChecker(this.positiveValue) == negativeValue, "This value should be negative");
     }
 
     @Test
-    void invalidAbsolutIntChecker() {
-        // -5 < -1
-        assertTrue(absolutValueChecker.absolutIntChecker(this.invalidValue) <= negativeValue, "This value is invalid!");
-        // -5 >= 1
-        assertFalse(absolutValueChecker.absolutIntChecker(this.invalidValue) >= positiveValue, "This value is invalid!");
+    void negativeAbsolutIntChecker() {
+        // 5 != -5
+        assertTrue(absolutValueChecker.absolutIntChecker(this.negativeValue) != negativeValue, "This value should be positive!");
+        // -5 == 5
+        assertFalse(absolutValueChecker.absolutIntChecker(this.negativeValue) == negativeValue, "This value should be positive!");
     }
 }
